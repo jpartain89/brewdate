3750bb3a4b77d7f1af5ec297628e3cc1da5cfab6
jpartain89 <jpartain89@gmail.com>
2017-10-05 22:13:07 -0500

    Updates

--- brewdate.py
+++ brewdate.py
@@ -1,21 +1,28 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import subprocess
 
 def brewdate(command):
-    "This is simply calling brew with the command as the variable"
+    """
+    Calls the brew command using the literal file location.
+    It tells you which command is running and returns the output, if there is any.
+    """
+
     for default_commands in command:
         print("\nRunning brew", default_commands, "\n")
         subprocess.check_call(['/usr/local/bin/brew', default_commands])
 
 def brewdate_cask(command):
-    "This is simply calling brew cask with the command as the variable"
+    """
+    Calls the brew cask command using the literal file location.
+    It tells you which command is running and returns the output, if there is any.
+    """
     for default_commands in command:
         print("\nRunning brew cask", default_commands, "\n")
         subprocess.check_call(['/usr/local/bin/brew', 'cask', default_commands])
 
-thelist = ['update', 'upgrade', 'cleanup', 'prune', 'doctor']
-casklist = ['doctor', 'cleanup']
+THE_LIST = ['update', 'upgrade', 'cleanup', 'prune', 'doctor']
+CASK_LIST = ['doctor', 'cleanup']
 
-brewdate(thelist)
-brewdate_cask(casklist)
+brewdate(THE_LIST)
+brewdate_cask(CASK_LIST)
